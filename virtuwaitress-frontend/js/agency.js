529b012cd43e2dcaf32e7f10e344a305fa83bcc3
MisShellyMac <michellemccarthy@hotmail.com>
2015-10-12 09:36:02 -0700

    Front End Bootstrap Template for Index.html

---
+++ virtuwaitress-frontend/js/agency.js
@@ -0,0 +1,71 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+var interval = null;
+
+function turnButtonOff()
+{
+    // Stop the flashing
+    clearInterval(interval);
+
+    // Make sure it's off
+    $('#callButtonOn').css('z-index', -100);
+    
+    interval = null;
+}
+
+function turnButtonOn()
+{
+    // Make the button flash
+
+    // Instantly turn it on
+    $('#callButtonOn').css('z-index', 100);
+
+    // Then alternate between on and off
+    interval = setInterval(function () {
+        if ($('#callButtonOn').css('z-index') == 100)
+        {
+            $('#callButtonOn').css('z-index', -100);
+        }
+        else
+        {
+            $('#callButtonOn').css('z-index', 100);
+        }
+    }, 700);   
+}
+
+function toggleButton()
+{
+    if (interval == null)
+    {
+        turnButtonOn();
+    }
+    else
+    {
+        turnButtonOff();
+    }
+}
