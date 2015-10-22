b658f311cdb4a3ee785bb7db3e171e21f34f45dc
MisShellyMac <michellemccarthy@hotmail.com>
2015-10-21 17:00:18 -0700

    WIP added js/approvedSocialMedia.js to public/js

--- routes/approvedSocialMedia.js
+++ routes/approvedSocialMedia.js
@@ -24,4 +24,8 @@ router.delete('/:id', function(req, res, next) {
   return controller.delete(req.params.id, res);
 });
 
+// router.get('/approvedSocialMedia', function(req, res, next){
+//   return controller.findAll(res);
+// });
+
 module.exports = router;
